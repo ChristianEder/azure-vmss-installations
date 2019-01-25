9d7eda11bf80d966b4cee19dbd3d946c6c9db946
ChristianEder <christian.johann.eder@googlemail.com>
2019-01-25 15:28:26 +0100

    install choco only if required

--- net472.ps1
+++ net472.ps1
@@ -1,4 +1,12 @@
 # install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+try 
+{
+  # if chocolatey is already installed, this will just work, otherwise an expection will be thrown, installing choco in the catch block
+  $installed = choco
+}
+catch
+{
+  Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 # install .NET 4.7.2 developer pack
 choco install netfx-4.7.2-devpack
